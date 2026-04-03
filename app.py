e78a6a69effc2f4130589609b3868bc499d92cae
Gorinta Harshitha <gorintaharshitha@gmail.com>
2026-04-03 10:05:39 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import joblib
+import streamlit as st
+model=joblib.load("model.pkl")
+vectorizer=joblib.load("vectorizer.pkl")
+
+
+st.title("Fake News Detection App")
+user_input=st.text_area("Enter news..")
+if st.button("Click News"):
+    input_data=vectorizer.transform([user_input])
+    prediction=model.predict(input_data)
+    if prediction[0]==0:
+        st.error("Fake News")
+    else:
+        st.success("Real News")
