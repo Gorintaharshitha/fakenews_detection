ddcde7f5f291fa58e9a1fa5a98cf18681aff1e81
Harshi <gorintaharshitha@gmail.com>
2026-06-22 11:39:18 +0530

    information added

--- fakenews_detection/app.py
+++ fakenews_detection/app.py
@@ -6,7 +6,10 @@ BASE_DIR =os.path.dirname(os.path.abspath(__file__))
 model = joblib.load(os.path.join(BASE_DIR, "model.pkl"))
 vectorizer = joblib.load(os.path.join(BASE_DIR, "vectorizer.pkl"))
 st.title("Fake News Detection-web Application")
-st.info(""" Instruction:\n1.Enter a complete news article.\n2.Avoid short Sentences.\n3.click the 'check news button' to seethe result.""")
+st.info(""" Instructions:
+        1.Enter a complete news article.
+        2.Avoid short Sentences.
+        3.click the 'check news button' to seethe result.""")
 user_input=st.text_area("Enter news article:")
 if st.button("Check News"):
     if user_input.strip()=="":
